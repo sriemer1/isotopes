715fa1de31ab3cf2cda1b36d01b0f78172c52ce0
Sydney Riemer <sriemer1@jhu.edu>
2015-07-06 17:39:01 -0400

    first version on git

---
+++ isotopes.py
@@ -0,0 +1,91 @@
+import csv
+import os
+import matplotlib.pyplot as plt
+from pylab import *
+import numpy as numpy
+import math
+import sys
+from matplotlib import *
+import io
+
+owd = os.getcwd()  #gets original working directory where allmol file is
+
+#initializes lists for masses, #C, #N, #O, and #H from allmol file
+masses = [] 
+numC = []
+numN = []
+numO = []
+numH = []
+#lists to store unsorted data
+data1 = []
+txt = []
+
+with open("allmol_sort.txt", 'rb') as f1:     #opens file and reads it in
+    for i in xrange(1):
+        f1.next()       #skips header at top of .txt file
+    data = csv.reader(f1, delimiter='\t')
+    for column in data:
+        data1.append(column[0])   #adds all data from file
+
+for i in range(0, len(data1)): #makes all data into separate numbers for list processing
+    txt.append(data1[i])
+newData= " ".join(txt)  
+nd1 = newData.split()
+
+"""
+    Adds elements from list nd1 to a new list based on start index and step. 
+    To be used to make list of masses, #C, #N, #O, #H. Step size of 5 is used
+    to separate mass, C, N, O, H.
+    @param list_name    name of the list to append data to
+    @param start    index to start reading data from in list nd1
+"""
+def addToList(list_name, start):
+    for i in range(start, len(nd1), 5):   
+        list_name.append(nd1[i])
+
+#adds data to lists calling addToList function
+addToList(masses, 0)  
+addToList(numC, 1)
+addToList(numN, 2)
+addToList(numO, 3)
+addToList(numH, 4)
+
+os.chdir("Mass spec data tables")  #changes directories to where file with processed RGA data is
+os.chdir("Data tables 20sccm 9 torr 1")
+
+##### Collecting masses from RGA data #####
+with open("2015-06-19 16-02-35 9 torr 0.05_peak_differences 1.txt") as f2: #opens RGA data file
+    f2.next()
+    columns = csv.reader(f2, delimiter='\t')
+    mass_old = []
+    for i in columns:
+        mass_old.append(i[0])
+    nm1 = " ".join(mass_old)
+    nm2 = nm1.split()
+    mass_old1 = []   
+    for i in range(0, len(nm2), 2):
+        mass_old1.append(nm2[i])
+os.chdir(owd) #back to original working directory (wherever allmol file is)
+
+possible_mols = [] #list to store possible molecules
+for i in mass_old1:
+    for j in range(365): #365 is used because that is how many masses between 1 and 100 in allmol file
+        if float(i)<=float(masses[j])+1 and float(i)>=float(masses[j])-1:
+            possible_mols.append(i+ '     '+ masses[j]+'     C'+numC[j]+'N'+numN[j]+'O'+numO[j]+'H'+numH[j]) 
+
+#creates new file with possible molecular weights and formulas for given RGA mass
+name = "newfile2.txt"  #output file name 
+w = open(name, 'w')
+w.write("RGA Mass |  MW     | Formula\n")
+for i in possible_mols:
+    w.writelines("%s\n" % i)
+w.close()   
+        
+
+    
+
+
+
+
+       
+
