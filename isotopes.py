50ec1f3c7dd1af26a6c323bb84c036de3e585a38
Sydney Riemer <sriemer1@jhu.edu>
2015-07-10 16:56:57 -0400

    added new isotope stuff, still need to test it and apply to other element combos

--- isotopes.py
+++ isotopes.py
@@ -125,7 +125,29 @@ def isAround(intensity1, intensity2, abundance):
         return False
 
 for i in range(len(isotope_masses)):
-    if not noCarbon(isotope_masses[i]):
+    if not noCarbon(mass[i]) and not noNitrogen(mass[i]) and not noOxygen(mass[i]):
+        multiplyByC = int(molecule[i[0:2]])
+        multiplyByN = int(molecule[i[2:4]])
+        multiplyByO = int(molecule[i[4:6]])
+        multiplyByH = int(molecule[i[6:]])
+        if ((12.0107*multiplyByC)+(14.00674*multiplyByN)+(15.994915*multiplyByO)+(1.00794*multiplyByH)) == isotope_masses[i]+.5 or isotope_masses[i]-.5:
+            if mass[i+1]/mass[i] == multiplyByC * .0107:
+                file_isotopes.append("Isotope is: C-13")
+            elif mass[i+1]/mass[i] == multiplyByN * .00364:
+                file_isotopes.append("Isotope: is N-15")
+            elif mass[i+1]/mass[i] == multiplyByO * .00038:
+                file_isotopes.append("Isotope is: O-17")
+            elif mass[i+1]/mass[i] == multiplyByO * .00205:
+                file_isotopes.append("Isotope is: O-18")
+            else:
+                file_isotopes.append("Mix of isotopes")
+    elif not noCarbon(mass[i]) and not noNitrogen(mass[i]):
+        
+    elif not noNitrogen(mass[i]) and not noOxygen(mass[i]):
+        
+    elif not noCarbon(mass[i]) and not noOxygen(mass[i]):
+    
+    elif not noCarbon(isotope_masses[i]):
         multiplyByC = int(molecules[i[0:2]])
         if (12*multiplyBy)+1 or (12*multiplyBy)+1.1 or (12*multiplyBy)+1.2 or (12*multiplyBy)+.9 in isotope_masses:
             if isAround(intensities[mass_old1.index(mass)], intensities[(mass_old1.index(mass))+1], abundances[3]*multiplyBy):
